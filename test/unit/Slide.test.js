abfbfe2af286d4d0ee0dbb6f374896684ea441ed
ghaiklor <ghaiklor@gmail.com>
2016-01-19 16:59:42 +0200

    test(animation): Add missing tests

--- test/unit/Slide.test.js
+++ test/unit/Slide.test.js
@@ -4,16 +4,90 @@ import Rectangle from 'kittik-shape-rectangle';
 import Slide from '../../src/Slide';
 
 describe('Animation::Slide', () => {
+  it('Should properly get/set direction', () => {
+    const animation = new Slide({direction: 'inLeft'});
+    assert.equal(animation.getDirection(), 'inLeft');
+    assert.instanceOf(animation.setDirection('inRight'), Slide);
+    assert.equal(animation.getDirection(), 'inRight');
+  });
+
+  it('Should properly throw error if direction is not supports', () => {
+    const animation = new Slide();
+    assert.throws(() => animation.setDirection('wrong'), Error, 'Unknown direction: wrong');
+  });
+
+  it('Should properly parse coordinates for shape based on direction', () => {
+    const animation = new Slide();
+    const shape = new Rectangle();
+
+    assert.instanceOf(animation.setDirection('inUp'), Slide);
+    assert.deepEqual(animation._parseCoordinates(shape), {startX: 10, startY: -5, endX: 10, endY: 10});
+
+    assert.instanceOf(animation.setDirection('inDown'), Slide);
+    assert.deepEqual(animation._parseCoordinates(shape), {
+      startX: 10,
+      startY: process.stdout.rows + 5,
+      endX: 10,
+      endY: 10
+    });
+
+    assert.instanceOf(animation.setDirection('inLeft'), Slide);
+    assert.deepEqual(animation._parseCoordinates(shape), {startX: -15, startY: 10, endX: 10, endY: 10});
+
+    assert.instanceOf(animation.setDirection('inRight'), Slide);
+    assert.deepEqual(animation._parseCoordinates(shape), {
+      startX: process.stdout.columns + 15,
+      startY: 10,
+      endX: 10,
+      endY: 10
+    });
+
+    assert.instanceOf(animation.setDirection('outUp'), Slide);
+    assert.deepEqual(animation._parseCoordinates(shape), {startX: 10, startY: 10, endX: 10, endY: -5});
+
+    assert.instanceOf(animation.setDirection('outDown'), Slide);
+    assert.deepEqual(animation._parseCoordinates(shape), {
+      startX: 10,
+      startY: 10,
+      endX: 10,
+      endY: process.stdout.rows + 5
+    });
+
+    assert.instanceOf(animation.setDirection('outLeft'), Slide);
+    assert.deepEqual(animation._parseCoordinates(shape), {startX: 10, startY: 10, endX: -15, endY: 10});
+
+    assert.instanceOf(animation.setDirection('outRight'), Slide);
+    assert.deepEqual(animation._parseCoordinates(shape), {
+      startX: 10,
+      startY: 10,
+      endX: process.stdout.columns,
+      endY: 10
+    });
+  });
+
   it('Should properly call the animate() method', done => {
-    let animation = new Slide();
-    let shape = new Rectangle();
-    let mock = sinon.mock(animation);
+    const animation = new Slide();
+    const shape = new Rectangle();
+    const mock = sinon.mock(animation);
 
     mock.expects('animateProperty').twice().returns(Promise.resolve());
 
     animation.animate(shape).then(() => {
       mock.verify();
       done();
+    });
+  });
+
+  it('Should properly serialize animation to Object', () => {
+    const animation = new Slide();
+
+    assert.deepEqual(animation.toObject(), {
+      type: 'Slide',
+      options: {
+        duration: 1000,
+        easing: 'outQuad',
+        direction: 'inRight'
+      }
     });
   });
 });
